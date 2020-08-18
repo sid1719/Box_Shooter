dd2d50ce80f06dd20451be30e6fe2f80abaf66cd
Siddharth Prabhu <siddharthprabhu30@gmail.com>
2020-08-19 00:14:01 +0530

    Assets

---
+++ Assets/Scripts/basiccontroller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basiccontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Horizontal Input=" + Input.GetAxis("Horizontal"));
+    }
+}
---
+++ Assets/Scripts/basicmover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class basicmover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float spinSpeed = 180.0f;
+    public float motionMagni = 0.1f;
+    public bool doSpin = true;
+    public bool doMotion = true;
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate around y axis
+        if (doSpin)
+        { gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime); }
+        //move up and down
+        if (doMotion)
+        { gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagni); }
+    }
+}
